1a1dceb9a9f8ed13a7ef83c2ae5c0afd3f99b426
kissmycasio <kissmycasio@proton.me>
2026-03-26 04:55:44 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const params = new URLSearchParams(window.location.search);
+const productsParam = params.get("products");
+
+const itemsDiv = document.getElementById("items");
+const checkoutBtn = document.getElementById("checkoutBtn");
+
+fetch("products.json")
+  .then(res => res.json())
+  .then(productMap => {
+
+    let redirectUrl = productMap["default"].url;
+
+    if (productsParam) {
+      const entries = productsParam.split(",");
+
+      entries.forEach(entry => {
+        const [id, qty] = entry.split(":");
+        const quantity = parseInt(qty) || 1;
+
+        const product = productMap[id];
+
+        if (product) {
+          const div = document.createElement("div");
+          div.className = "item";
+
+          div.innerHTML = `
+            <img src="${product.image}">
+            ${product.name} (Qty: ${quantity})
+          `;
+
+          itemsDiv.appendChild(div);
+
+          // First valid product becomes checkout link
+          if (redirectUrl === productMap["default"].url) {
+            redirectUrl = product.url;
+          }
+        }
+      });
+    }
+
+    checkoutBtn.href = redirectUrl;
+
+  })
+  .catch(() => {
+    checkoutBtn.href = "/";
+  });
